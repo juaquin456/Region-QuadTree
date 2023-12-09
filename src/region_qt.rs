4272f204e2f146a7c51da26f638e7c8519623738
juaquin456 <juaquin.remon@utec.edu.pe>
2023-12-09 18:51:15 -0500

    reformat Color enum for save data Rgba

--- src/region_qt.rs
+++ src/region_qt.rs
@@ -1,29 +1,21 @@
 use std::rc::Rc;
 
 use image::io::Reader as ImageReader;
-use image::{DynamicImage, GenericImageView};
+use image::{DynamicImage, GenericImageView, Rgba};
 
 use primitives::BoundingBox;
-use crate::region_qt::Color::{Black, White};
 
 use crate::region_qt::primitives::Point;
 mod primitives;
 
 #[derive(PartialEq)]
 pub enum Color {
-    Black,
     Gray,
-    White,
+    Data(Rgba<u8>),
 }
 
 fn get_color(img: &DynamicImage, coord: (u32, u32)) -> Color {
-    let real_color = img.get_pixel(coord.0, coord.1);
-    if real_color[0] == 0 {
-        Black
-    }
-    else {
-        White
-    }
+    Color::Data(img.get_pixel(coord.0, coord.1))
 }
 
 struct RegionNodeQt {
@@ -33,11 +25,28 @@ struct RegionNodeQt {
 }
 
 impl RegionNodeQt {
+    fn new(min: Point, max: Point) -> Self {
+        RegionNodeQt {
+            data: Color::Gray,
+            bounding: BoundingBox::new(min, max),
+            children: [None, None, None, None],
+        }
+    }
+
+    fn is_leaf(&self) -> bool {
+        for i in 0..4 {
+            if !self.children[i].is_none() {
+                return false;
+            }
+        }
+        true
+    }
+
     fn calculate_color(&self, img: &DynamicImage) -> Color {
         let xl = self.bounding.min().x;
         let yl = self.bounding.min().y;
 
-        let mut current_color = get_color(img, (xl, yl));
+        let current_color = get_color(img, (xl, yl));
 
         for x in xl..self.bounding.max().x {
             for y in self.bounding.min().y..self.bounding.max().y {
@@ -51,7 +60,18 @@ impl RegionNodeQt {
     }
 
     fn update(&mut self, img: &DynamicImage) {
-
+        let color = self.calculate_color(img);
+        match color {
+            Color::Gray => {
+                if self.is_leaf() {
+                    self.initialize_children();
+                    for i in 0..4 {
+                        self.children[i].unwrap().update(img);
+                    }
+                }
+            }
+            _ => { self.data = color; }
+        }
     }
 }
 
@@ -69,13 +89,9 @@ impl RegionQt {
             .expect("Can't open the file")
             .decode()
             .unwrap();
-        let (width, height) = img.dimensions();
+        let dim = img.dimensions();
 
-        self.root = Some(Rc::new(RegionNodeQt {
-            data: Color::Black,
-            bounding: BoundingBox::new(Point::from((0, 0)), Point::from((width, height))),
-            children: [None, None, None, None],
-        }));
+        self.root = Some(Rc::new(RegionNodeQt::new(Point::from((0, 0)), Point::from(dim))));
 
         Rc::get_mut(self.root.as_mut().unwrap()).unwrap().update(&img);
     }
