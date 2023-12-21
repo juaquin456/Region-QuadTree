c42b48edf0e7fc4b15b49ac225e6b6ff139afbb4
juaquin456 <juaquin.remon@utec.edu.pe>
2023-12-21 12:14:51 -0500

    update examples

--- examples/plotting.rs
+++ examples/plotting.rs
@@ -2,6 +2,7 @@ use region_quadtree::region_qt;
 
 fn main() {
     let mut tree = region_qt::RegionQt::new();
-    tree.build("img/test3.png");
+    tree.build("img/GH.png");
     tree.plot();
+    tree.write("test.bin");
 }
